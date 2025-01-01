a6470b2f9f7eeeefb9ff6887d4595fbaee8405ed
alsry922 <alsry922@gmail.com>
2025-01-02 00:13:10 +0900

    [44]

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+
+
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-user',
@@ -8,5 +15,22 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  // @Input({required: true}) id!: string;
+  // @Input({required: true}) avatar!: string;
+  // @Input({required: true}) name!: string;
+  @Input({required: true}) user!: User;
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+  // imagePath = computed(() => `assets/users/${this.avatar}`)
+  @Output() select = new EventEmitter<string>();
 
+  // select = output<string>()
+
+  get imagePath() {
+    return `assets/users/${this.user.avatar}`;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
 }
